a6b4c5a3d75fbe6253edc0c519c562d39725e906
lamech_tenny <69479491+lamechtenny@users.noreply.github.com>
2023-05-21 15:11:45 +0530

    exor_test.py

---
+++ tests/exor_test.py
@@ -0,0 +1,45 @@
+#----------------module import------------------
+
+#coco testbench py frame work
+import cocotb
+
+#synchronisation and event handling
+#trigger classes used for defining
+#events to wait for during simulation.
+from cocotb.triggers import Timer, RisingEdge
+
+
+#-----------Defining the Test Function------------
+
+#decorator is used to define the test function
+@cocotb.test()
+
+#test function name = or_test  , dut as parameter
+async def exor_test(dut):
+    
+    #expected input a,b posiibilties in a tuple(immutable)
+    a=(0,0,1,1)
+    b=(0,1,0,1)
+
+    #expected output y posiibilties
+    y=(0,1,1,0)
+    
+    #Test verification y , iteration of a ,b
+    for i in range(4):
+        dut.a.value = a[i]
+        dut.b.value = b[i]
+        
+        
+        #wait for 1 nanosecond before proceeding to
+        #allow the DUT to process the in and produce out
+        await Timer(1,'ns')
+        
+        #An assertion statement
+        #If the assertion fails, it raises an error with a message
+        assert dut.y.value == y[i] ,f"Error @ iter {i}"
+        
+        
+#SUMMARY      
+# Overall, this code sets up a testbench that verifies the functionality of an OR gate by applying different input combinations
+# and comparing the actual output with the expected output.
+   
