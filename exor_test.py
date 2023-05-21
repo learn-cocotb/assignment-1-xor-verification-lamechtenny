e3515cde347826e55827768c54fb704ce4fd3eb9
lamech_tenny <69479491+lamechtenny@users.noreply.github.com>
2023-05-21 15:09:59 +0530

    exor_test.py

--- exor_test.py
+++ exor_test.py
@@ -15,7 +15,7 @@ from cocotb.triggers import Timer, RisingEdge
 @cocotb.test()
 
 #test function name = or_test  , dut as parameter
-async def xor_test(dut):
+async def exor_test(dut):
     
     #expected input a,b posiibilties in a tuple(immutable)
     a=(0,0,1,1)
